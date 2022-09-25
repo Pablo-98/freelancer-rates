54b893e80cfad0bf7d75fefb4d2d99afeea196bb
Pablo Hernandez <hpablo575@gmail.com>
2022-09-24 20:03:03 -0600

    declared pricewithMonthlyDiscount function

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
 // given a payrate calculate a workday (8hrs)
-function dayRate(rate) {
+ function dayRate(rate) {
     return rate *8
 }
-console.log(dayRate(100));
 
 // calculate number of workdays given a budget
 
-function daysInBudget(budget, rate){
+ function daysInBudget(budget, rate){
     return budget/ dayRate(rate);
 }
 
-console.log(daysInBudget(1600,100));
 
 //calculate the discount rate for large projects
 // the freelancer offers a discount for every full month. The rest are billed at a dayrate
 
-function priceWithMonthlyDiscount(rate, numDays, discount){
+ function priceWithMonthlyDiscount(rate, numDays, discount){
     // discount is 42%(0.42)
-    return 
+    //step 1 divide total days by 22
+    // step 2 multiply the regular dayRate by 22 to calculate how much that month is going to cost 
+    // step 3  Multiply .42 * monthlyRate 
+    // step 4 subtract the monthlyRate - the number from step 3
+    // step 5 multiply the discounted monthly rate by the number of months that are going to be worked 
+    // step 6 multiply the remaining days at the regular dayRate and add it to step 5
+    const totalMonths = Math.floor(numDays / 22);
+    const monthlyRate = rate * 8 * 22; 
+    const remainingDays = numDays % 22 //% symbol returns the remainder which in this case is 10
+    const discountedMonth = monthlyRate - monthlyRate * discount;
+    
+
+    return discountedMonth * totalMonths + remainingDays * dayRate(rate);
+
+
 }
-console.log(dayRate(89))
+console.log(dayRate(89,230,0.42));
+// returns 712 when the answer should be 97972
